6fd9495309e25f596520aa27c3a32022f37a4b03
Anthony Loeza <aloeza1@cps.edu>
2025-04-24 10:52:33 -0500

    ohyeah

--- neural_test.py
+++ neural_test.py
@@ -21,9 +21,9 @@ rn_training_data = [
 nn = NeuralNet(6, 1, 1)
 nn.train(rn_training_data)
 
-print(nn.get_ih_weights())
+print(f"weights:{nn.get_ih_weights()}")
 print()
-print(nn.get_ho_weights())
+print(f"Weights:{nn.get_ho_weights()}")
 
 print(nn.evaluate([1, 1, 1, 1, 1, 1]))
 print(nn.evaluate([0, 0, 0, 0, 0, 0]))
@@ -115,6 +115,28 @@ print(sqn.evaluate([0.95]))
 print("\n\nTraining XOR\n\n")
 xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 
-xorn = NeuralNet(2, 1, 1)
+xorn = NeuralNet(2,1,1)
 xorn.train(xor_training_data)
 print(xorn.test_with_expected(xor_training_data))
+
+test_training_data =  [
+    ([.9,.8,.6,.3,.1],[1]),
+    ([.8,.8,.4,.6,.4],[1]),
+    ([.7,.2,.4,.6,.3],[1]),
+    ([.5,.5,.8,.4,.8],[0]),
+    ([.3,.1,.6,.8,.8],[0]),
+    ([.6,.3,.4,.3,.6],[0])
+]
+test_data = [
+    ([1,1,1,.1,.1]),
+    ([.5,.2,.1,.7,.7]),
+    ([.8,.3,.3,.3,.8]),
+    ([.8,.3,.3,.8,.3]),
+    ([.9,.8,.8,.3,.6])
+]
+test = NeuralNet(5,8,1)
+test.train(test_training_data)
+print(f"Weights:{test.get_ho_weights()}")
+print(test.test_with_expected(test_training_data))
+for person in test_data:
+    print(round((test.evaluate(person)[0])))
